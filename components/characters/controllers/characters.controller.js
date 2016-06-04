f33e294131d929f318b43a83cd4ec739880c3405
Lee Brandt <leebrandt@gmail.com>
2016-06-04 03:49:15 -0500

    Addes some stylin

--- components/characters/controllers/characters.controller.js
+++ components/characters/controllers/characters.controller.js
@@ -2,11 +2,17 @@
   
   var characterCtrl = function(CharacterSvc){
     var ctrl = this;
+    ctrl.pageSizeOptions = [3, 5, 10];
+    ctrl.pageSize = 3;
 
     ctrl.loadCharacters = function(){
       ctrl.characters = CharacterSvc.LoadCharacters().$$state;  
     };
     
+    ctrl.repeatPageSize = function(){
+      return new Array(ctrl.pageSize);
+    };
+
     return ctrl;
   };
   
--- components/common/directives/placeholder.directive.js
+++ components/common/directives/placeholder.directive.js
@@ -7,7 +7,7 @@
 				for (var i = attrs.numberOfRows - 1; i >= 0; i--) {
 					var row = angular.element('<li class="placeholder" ng-if="!'+attrs.hidePlaceholders+'"></li>');
 					for (var c = attrs.numberOfCells - 1; c >= 0; c--) {
-						row.append(angular.element('<span>########</span>'));		
+						row.append(angular.element('<span></span>'));		
 					}
 					el.append($compile(row)(scope));
 		 		}
